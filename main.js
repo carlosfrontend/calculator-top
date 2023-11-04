f229a0937ab69b705f5c1627df7b0ff60b8eeae3
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2023-11-05 00:36:44 +0100

    Feat: Evaluate a pair of numbers

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const display = document.querySelector('.display-text');
 const clearButton = document.querySelector('.ac');
 const decimal = document.querySelector('.decimal');
 
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+let result = 0;
 
 function add(a, b) {
   return a + b;
@@ -23,10 +27,6 @@ function divide(a, b) {
   return a / b;
 }
 
-let firstNumber = '';
-let operator = '';
-let secondNumber = '';
-let result = 0;
 
 // Create a function named operate that take a operator parameter and a num1 and num2 parameters and then call to one of the above functions in the numbers
 
@@ -50,74 +50,48 @@ function operate(operator, num1, num2) {
   }
 }
 
-let displayData = '';
+let tempValue = '';
 
-let showFirstOperand = (num) => {
-  displayData = num;
-  placeholder.textContent = displayData;
-  displayData = '';
-}
-
-let showOperator = (num1, operator) => {
-  placeholder.textContent = num1 + operator;
-}
-
-let showSecondOperand = (num1, operator, num2) => {
-  placeholder.textContent = num1 + operator + num2;
-}
-
-let showResult = (result) => {
-  display.textContent = result;
-}
+/* function showData(data) {
+  tempValue = data;
+  placeholder.textContent += tempValue;
+  return placeholder.textContent;
+} */
 
 
+display.textContent = '=0'
 
 buttons.forEach(button => button.addEventListener('click', () => {
-  let btn = button.textContent;
-  let temp = '';
-  if (operator === '' && btn !== 'AC' && btn !== 'C' && btn !== '=' && btn !== '+' && btn !== '-' && btn !== 'x' && btn !== '/') {
-    temp = btn;
-    firstNumber += temp;
-    console.log(+firstNumber);
-    temp = '';
-    showFirstOperand(firstNumber);
+
+  let displayValue = '';
+  switch (button.className) {
+    case 'operator':
+      displayValue = button.textContent;
+      operator = displayValue;;
+      console.log(operator);
+      placeholder.textContent += displayValue;
+      break;
+    case 'number':
+      if (operator === '') {
+        displayValue = button.textContent;
+        firstNumber += displayValue;
+        console.log(firstNumber);
+        placeholder.textContent += displayValue;
+        break;
+      }
+    case 'number':
+      if (firstNumber !== '' && operator !== '') {
+        displayValue = button.textContent;
+        secondNumber += displayValue;
+        console.log(secondNumber);
+        placeholder.textContent += displayValue;
+        result = operate(operator, +firstNumber, +secondNumber);
+        display.textContent = '=' + result;
+        console.log(`First Number: ${+firstNumber} Operator: ${operator} Second Number: ${+secondNumber}`)
+        break;
+      }
   }
 
-  if (operator === '' && btn === '+' || btn === '-' || btn === 'x' || btn === '/') {
-    temp = btn;
-    operator = temp;
-    console.log(operator);
-    temp = '';
-    showOperator(firstNumber, operator)
-  }
 
-  if (operator !== '' && btn !== 'AC' && btn !== 'C' && btn !== '=' && btn !== '+' && btn !== '-' && btn !== 'x' && btn !== '/') {
-    temp = btn;
-    secondNumber += temp;
-    console.log(+secondNumber);
-    temp = '';
-    showSecondOperand(firstNumber, operator, secondNumber);
-  }
 
-  if(btn == '='){
-    display.textContent = firstNumber;
-  }
-
-  if(firstNumber !== '' && operator !== '' && secondNumber !== '' && btn === '='){
-    temp = operate(operator, +firstNumber, +secondNumber);
-    firstNumber = temp;
-    result = firstNumber;
-    showResult(result);
-    firstNumber = result;
-    secondNumber = '';
-    showOperator(firstNumber, operator);
-    showSecondOperand(firstNumber, operator, secondNumber);
-  }  
 }));
-
-
-
-
-
-
-/* numbers.forEach(number => number.removeEventListener('click',_handleFirstOperand)); Works fine! */
